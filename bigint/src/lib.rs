0306061cb19532fb6a48f61c7e7401b23428de44
Wei Tang <hi@that.world>
2017-07-02 22:18:20 +0800

    bigint: fork and modify Parity's hash mod

--- bigint/src/lib.rs
+++ bigint/src/lib.rs
@@ -2,16 +2,20 @@
 
 extern crate etcommon_rlp as rlp;
 extern crate etcommon_util as util;
+extern crate rand;
+extern crate libc;
 extern crate byteorder;
 #[macro_use] extern crate heapsize;
 
 mod m256;
 mod mi256;
 mod uint;
+mod hash;
 
 pub use self::uint::{U128, U256, U512};
 pub use self::m256::M256;
 pub use self::mi256::MI256;
+pub use self::hash::{H32, H64, H128, H160, H256, H264, H512, H520, H1024, H2048};
 
 #[derive(Eq, PartialEq, Debug, Copy, Clone, Hash)]
 /// Sign of an integer.
