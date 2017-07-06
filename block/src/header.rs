a4e29cabc0ab62ec1e10a32d10019c5ebeb8b6a7
Wei Tang <hi@that.world>
2017-07-06 15:19:44 +0800

    block: rlp impl for Header and PartialHeader

--- block/src/header.rs
+++ block/src/header.rs
@@ -1,3 +1,4 @@
+use rlp::{Encodable, Decodable, RlpStream, DecoderError, UntrustedRlp};
 use bigint::{Address, LogsBloom, Gas, H256, U256, B256};
 
 pub struct Header {
@@ -16,4 +17,143 @@ pub struct Header {
     pub extra_data: B256,
     pub mix_hash: H256,
     pub nonce: u64,
+}
+
+impl Header {
+    pub fn from_partial(header: PartialHeader, mix_hash: H256, nonce: u64) -> Self {
+        Self {
+            mix_hash, nonce,
+            parent_hash: header.parent_hash,
+            ommers_hash: header.ommers_hash,
+            beneficiary: header.beneficiary,
+            state_root: header.state_root,
+            transactions_root: header.transactions_root,
+            receipts_root: header.receipts_root,
+            logs_bloom: header.logs_bloom,
+            difficulty: header.difficulty,
+            number: header.number,
+            gas_limit: header.gas_limit,
+            gas_used: header.gas_used,
+            timestamp: header.timestamp,
+            extra_data: header.extra_data,
+        }
+    }
+}
+
+impl Encodable for Header {
+    fn rlp_append(&self, s: &mut RlpStream) {
+        s.begin_list(15);
+        s.append(&self.parent_hash);
+        s.append(&self.ommers_hash);
+        s.append(&self.beneficiary);
+        s.append(&self.state_root);
+        s.append(&self.transactions_root);
+        s.append(&self.receipts_root);
+        s.append(&self.logs_bloom);
+        s.append(&self.difficulty);
+        s.append(&self.number);
+        s.append(&self.gas_limit);
+        s.append(&self.gas_used);
+        s.append(&self.timestamp);
+        s.append(&self.extra_data);
+        s.append(&self.mix_hash);
+        s.append(&self.nonce);
+    }
+}
+
+impl Decodable for Header {
+    fn decode(rlp: &UntrustedRlp) -> Result<Self, DecoderError> {
+        Ok(Self {
+            parent_hash: rlp.val_at(0)?,
+            ommers_hash: rlp.val_at(1)?,
+            beneficiary: rlp.val_at(2)?,
+            state_root: rlp.val_at(3)?,
+            transactions_root: rlp.val_at(4)?,
+            receipts_root: rlp.val_at(5)?,
+            logs_bloom: rlp.val_at(6)?,
+            difficulty: rlp.val_at(7)?,
+            number: rlp.val_at(8)?,
+            gas_limit: rlp.val_at(9)?,
+            gas_used: rlp.val_at(10)?,
+            timestamp: rlp.val_at(11)?,
+            extra_data: rlp.val_at(12)?,
+            mix_hash: rlp.val_at(13)?,
+            nonce: rlp.val_at(14)?,
+        })
+    }
+}
+
+pub struct PartialHeader {
+    pub parent_hash: H256,
+    pub ommers_hash: H256,
+    pub beneficiary: Address,
+    pub state_root: H256,
+    pub transactions_root: H256,
+    pub receipts_root: H256,
+    pub logs_bloom: LogsBloom,
+    pub difficulty: U256,
+    pub number: U256,
+    pub gas_limit: Gas,
+    pub gas_used: Gas,
+    pub timestamp: u64,
+    pub extra_data: B256,
+}
+
+impl PartialHeader {
+    pub fn from_full(header: Header) -> Self {
+        Self {
+            parent_hash: header.parent_hash,
+            ommers_hash: header.ommers_hash,
+            beneficiary: header.beneficiary,
+            state_root: header.state_root,
+            transactions_root: header.transactions_root,
+            receipts_root: header.receipts_root,
+            logs_bloom: header.logs_bloom,
+            difficulty: header.difficulty,
+            number: header.number,
+            gas_limit: header.gas_limit,
+            gas_used: header.gas_used,
+            timestamp: header.timestamp,
+            extra_data: header.extra_data,
+        }
+    }
+}
+
+impl Encodable for PartialHeader {
+    fn rlp_append(&self, s: &mut RlpStream) {
+        s.begin_list(13);
+        s.append(&self.parent_hash);
+        s.append(&self.ommers_hash);
+        s.append(&self.beneficiary);
+        s.append(&self.state_root);
+        s.append(&self.transactions_root);
+        s.append(&self.receipts_root);
+        s.append(&self.logs_bloom);
+        s.append(&self.difficulty);
+        s.append(&self.number);
+        s.append(&self.gas_limit);
+        s.append(&self.gas_used);
+        s.append(&self.timestamp);
+        s.append(&self.extra_data);
+    }
+}
+
+impl Decodable for PartialHeader {
+    fn decode(rlp: &UntrustedRlp) -> Result<Self, DecoderError> {
+        Ok(Self {
+            parent_hash: rlp.val_at(0)?,
+            ommers_hash: rlp.val_at(1)?,
+            beneficiary: rlp.val_at(2)?,
+            state_root: rlp.val_at(3)?,
+            transactions_root: rlp.val_at(4)?,
+            receipts_root: rlp.val_at(5)?,
+            logs_bloom: rlp.val_at(6)?,
+            difficulty: rlp.val_at(7)?,
+            number: rlp.val_at(8)?,
+            gas_limit: rlp.val_at(9)?,
+            gas_used: rlp.val_at(10)?,
+            timestamp: rlp.val_at(11)?,
+            extra_data: rlp.val_at(12)?,
+        })
+    }
 }
