bdd6f30140c5077fe262ddb0f5116f2e366f6463
Wei Tang <hi@that.world>
2017-07-06 18:23:11 +0800

    block: encode/decode the whole block RLP

--- block/src/lib.rs
+++ block/src/lib.rs
@@ -3,5 +3,9 @@ extern crate etcommon_util as util;
 extern crate etcommon_rlp as rlp;
 
 mod header;
+mod transaction;
+mod block;
 
+pub use transaction::{TransactionSignature, TransactionAction, Transaction};
 pub use header::Header;
+pub use block::Block;
