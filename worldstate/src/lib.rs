9c2e1c9004b792264483714d7eab7dafccc18ff5
Wei Tang <hi@that.world>
2017-07-30 23:14:26 +0800

    worldstate: migrate build for trie in database

--- worldstate/src/lib.rs
+++ worldstate/src/lib.rs
@@ -7,21 +7,124 @@ pub mod merkle;
 
 use bigint::H256;
 use rlp::Rlp;
+use crypto::keccak256;
+use std::collections::HashMap;
 use merkle::{NibbleSlice, MerkleValue, MerkleNode};
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
 use std::borrow::Borrow;
 use std::clone::Clone;
 
 pub trait Database {
-    fn get(&self, hash: H256) -> &[u8];
+    fn get<'a>(&'a self, hash: H256) -> &'a [u8];
+    fn set<'a, 'b>(&'a mut self, hash: H256, value: &'b [u8]);
 }
 
-pub struct Trie<D: Deref<Target=Database> + Clone> {
+pub struct Trie<D: Deref<Target=Database> + DerefMut<Target=Database> + Clone> {
     database: D,
     root: H256,
 }
 
-impl<D: Deref<Target=Database> + Clone> Trie<D> {
+impl<D: Deref<Target=Database> + DerefMut<Target=Database> + Clone> Trie<D> {
+    fn build_node<'a>(database: &mut D, map: &HashMap<NibbleSlice<'a>, &'a [u8]>) -> MerkleNode<'a> {
+        if map.len() == 0 {
+            panic!();
+        }
+
+        if map.len() == 1 {
+            let key = map.keys().next().unwrap();
+            return MerkleNode::Leaf(key.clone(), map.get(&key).unwrap().clone());
+        }
+
+        let common = {
+            let mut iter = map.keys();
+
+            let mut common = iter.next().unwrap().common(iter.next().unwrap());
+            for key in iter {
+                common = common.common(key);
+            }
+
+            common
+        };
+
+        if common.len() > 0 {
+            let mut sub_map = HashMap::new();
+            for (key, value) in map {
+                sub_map.insert(key.sub(common.len(), key.len()), value.clone());
+            }
+            debug_assert!(sub_map.len() > 0);
+            let node = Self::build_node(database, &sub_map);
+            let value = if node.inlinable() {
+                MerkleValue::Full(Box::new(node))
+            } else {
+                let sub_node = rlp::encode(&node).to_vec();
+                let hash = keccak256(&sub_node);
+                database.set(hash, &sub_node);
+                MerkleValue::Hash(hash)
+            };
+            return MerkleNode::Extension(common, value);
+        }
+
+        let mut nodes = [MerkleValue::Empty, MerkleValue::Empty,
+                         MerkleValue::Empty, MerkleValue::Empty,
+                         MerkleValue::Empty, MerkleValue::Empty,
+                         MerkleValue::Empty, MerkleValue::Empty,
+                         MerkleValue::Empty, MerkleValue::Empty,
+                         MerkleValue::Empty, MerkleValue::Empty,
+                         MerkleValue::Empty, MerkleValue::Empty,
+                         MerkleValue::Empty, MerkleValue::Empty];
+
+        for i in 0..16 {
+            let mut sub_map = HashMap::new();
+            for (key, value) in map {
+                if key.len() > 0 && key.at(0) == i as u8 {
+                    sub_map.insert(key.sub(1, key.len()), value.clone());
+                }
+            }
+            let value = if sub_map.len() == 0 {
+                MerkleValue::Empty
+            } else {
+                let node = Self::build_node(database, &sub_map);
+                if node.inlinable() {
+                    MerkleValue::Full(Box::new(node))
+                } else {
+                    let sub_node = rlp::encode(&node).to_vec();
+                    let hash = keccak256(&sub_node);
+                    database.set(hash, &sub_node);
+                    MerkleValue::Hash(hash)
+                }
+            };
+            nodes[i] = value;
+        }
+
+        let additional = {
+            let mut additional = None;
+            for (key, value) in map {
+                if key.len() == 0 {
+                    additional = Some(value.clone())
+                }
+            }
+            additional
+        };
+
+        return MerkleNode::Branch(nodes, additional);
+    }
+
+    pub fn build<'a>(mut database: D, map: &HashMap<&'a [u8], &'a [u8]>) -> Self {
+        let mut node_map = HashMap::new();
+
+        for (key, value) in map {
+            node_map.insert(NibbleSlice::new(key), value.clone());
+        }
+
+        let node = Self::build_node(&mut database, &node_map);
+        let hash = keccak256(&rlp::encode(&node).to_vec());
+
+        Trie {
+            database,
+            root: hash
+        }
+    }
+    
     fn get_by_value<'a, 'b>(&'a self, nibble: NibbleSlice<'b>, value: MerkleValue<'a>) -> Option<&'a [u8]> {
         match value {
             MerkleValue::Empty => None,
