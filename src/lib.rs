d9e7fcc4abebb2716278ea4b40b1ff2b2dbf45ab
Wei Tang <hi@that.world>
2017-08-04 11:19:56 +0800

    Adopt short lib name change for parent

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-pub extern crate etcommon_bigint as bigint;
-pub extern crate etcommon_rlp as rlp;
-pub extern crate etcommon_util as util;
-pub extern crate etcommon_block as block;
-pub extern crate etcommon_worldstate as worldstate;
+pub extern crate bigint;
+pub extern crate rlp;
+pub extern crate hexutil;
+pub extern crate block;
+pub extern crate bloom;
+pub extern crate trie;
