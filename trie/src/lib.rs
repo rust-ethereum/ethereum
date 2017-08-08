ab5b5003c3ee5b355f0e2b5a8614c2767f774d46
Wei Tang <hi@that.world>
2017-08-08 14:51:58 +0800

    trie: move tests to lib.rs

--- trie/src/lib.rs
+++ trie/src/lib.rs
@@ -596,6 +596,66 @@ mod tests {
     }
 
     #[test]
+    fn insert_middle_leaf() {
+        let mut database: HashMap<H256, Vec<u8>> = HashMap::new();
+        let mut trie = Trie::empty(database);
+
+        trie.insert("key1aa".as_bytes().into(),
+                    "0123456789012345678901234567890123456789xxx".as_bytes().into());
+        trie.insert("key1".as_bytes().into(),
+                    "0123456789012345678901234567890123456789Very_Long".as_bytes().into());
+        trie.insert("key2bb".as_bytes().into(),
+                    "aval3".as_bytes().into());
+        trie.insert("key2".as_bytes().into(),
+                    "short".as_bytes().into());
+        trie.insert("key3cc".as_bytes().into(),
+                    "aval3".as_bytes().into());
+        trie.insert("key3".as_bytes().into(),
+                    "1234567890123456789012345678901".as_bytes().into());
+        assert_eq!(trie.root(), H256::from_str("0xcb65032e2f76c48b82b5c24b3db8f670ce73982869d38cd39a624f23d62a9e89").unwrap());
+    }
+
+    #[test]
+    fn insert_single_item() {
+        let mut database: HashMap<H256, Vec<u8>> = HashMap::new();
+        let mut trie = Trie::empty(database);
+
+        trie.insert("A".as_bytes().into(),
+                    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".as_bytes().into());
+
+        assert_eq!(trie.root(), H256::from_str("0xd23786fb4a010da3ce639d66d5e904a11dbc02746d1ce25029e53290cabf28ab").unwrap());
+    }
+
+    #[test]
+    fn testy() {
+        let mut database: HashMap<H256, Vec<u8>> = HashMap::new();
+        let mut trie = Trie::empty(database);
+
+        trie.insert("test".as_bytes().into(),
+                    "test".as_bytes().into());
+        trie.insert("te".as_bytes().into(),
+                    "testy".as_bytes().into());
+
+        assert_eq!(trie.root(), H256::from_str("0x8452568af70d8d140f58d941338542f645fcca50094b20f3c3d8c3df49337928").unwrap());
+    }
+
+    #[test]
+    fn sub_genesis() {
+        let mut database: HashMap<H256, Vec<u8>> = HashMap::new();
+        let mut trie = Trie::empty(database);
+
+        let k1 = read_hex("0x204188718653cd7e50f3fd51a820db66112517ca190c637e7cdd80782d56").unwrap();
+        let v1 = vec![248, 78, 128, 138, 21, 45, 2, 199, 225, 74, 246, 128, 0, 0, 160, 86, 232, 31, 23, 27, 204, 85, 166, 255, 131, 69, 230, 146, 192, 248, 110, 91, 72, 224, 27, 153, 108, 173, 192, 1, 98, 47, 181, 227, 99, 180, 33, 160, 197, 210, 70, 1, 134, 247, 35, 60, 146, 126, 125, 178, 220, 199, 3, 192, 229, 0, 182, 83, 202, 130, 39, 59, 123, 250, 216, 4, 93, 133, 164, 112];
+        let k2 = read_hex("0xa390953f116afb00f89fbedb2f8e77297e4e7e1749e2ef0e32e17808e4ad").unwrap();
+        let v2 = vec![248, 77, 128, 137, 108, 107, 147, 91, 139, 189, 64, 0, 0, 160, 86, 232, 31, 23, 27, 204, 85, 166, 255, 131, 69, 230, 146, 192, 248, 110, 91, 72, 224, 27, 153, 108, 173, 192, 1, 98, 47, 181, 227, 99, 180, 33, 160, 197, 210, 70, 1, 134, 247, 35, 60, 146, 126, 125, 178, 220, 199, 3, 192, 229, 0, 182, 83, 202, 130, 39, 59, 123, 250, 216, 4, 93, 133, 164, 112];
+
+        trie.insert(k1, v1);
+        trie.insert(k2, v2);
+
+        assert_eq!(trie.root(), H256::from_str("bcb5ffb5c6c3e43ef07550fa30af86d66b4015ee3f64aaf70cd0bf8fcc60a9c6").unwrap());
+    }
+
+    #[test]
     fn trie_insert() {
         let mut map = HashMap::new();
 
