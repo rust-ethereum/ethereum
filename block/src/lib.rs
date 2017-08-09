d89a5bf12d735bf242d48497a5b98d549b907aab
Wei Tang <hi@that.world>
2017-08-09 12:29:38 +0800

    block: address from public or secret key

--- block/src/lib.rs
+++ block/src/lib.rs
@@ -1,6 +1,8 @@
 extern crate bigint;
 extern crate rlp;
 extern crate bloom;
+extern crate secp256k1;
+extern crate sha3;
 #[cfg(test)] extern crate hexutil;
 
 mod header;
@@ -9,6 +11,7 @@ mod block;
 mod account;
 mod receipt;
 mod log;
+mod address;
 
 pub use transaction::{TransactionSignature, TransactionAction, Transaction};
 pub use header::Header;
@@ -16,3 +19,4 @@ pub use block::Block;
 pub use account::Account;
 pub use receipt::Receipt;
 pub use log::Log;
+pub use address::FromKey;
