bfa269e0ccac51f1e007a658b2fe37d1ea774fd4
Wei Tang <hi@that.world>
2017-08-09 13:58:44 +0800

    trie: alias MemoryTrie

--- trie/src/lib.rs
+++ trie/src/lib.rs
@@ -39,6 +39,8 @@ fn empty_trie_hash() -> H256 {
     H256::from("0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421")
 }
 
+pub type MemoryTrie = Trie<HashMap<H256, Vec<u8>>>;
+
 pub struct Trie<D: Database> {
     database: D,
     root: H256,
