5ae790d277116aea7b3433a436e631654a16ce8f
Wei Tang <hi@that.world>
2017-12-29 23:45:22 +0800

    [WIP] Add back one of the old test

--- trie/src/memory.rs
+++ trie/src/memory.rs
@@ -41,3 +41,37 @@ impl SingletonMemoryTrieMut {
         self.root = new_root;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use trie_test::{DatabaseGuard, Trie};
+    use std::collections::HashMap;
+    use std::str::FromStr;
+    use std::cell::UnsafeCell;
+    use bigint::H256;
+    use hexutil::read_hex;
+
+    #[test]
+    fn trie_middle_leaf() {
+        let mut map = HashMap::new();
+        map.insert("key1aa".as_bytes().into(), "0123456789012345678901234567890123456789xxx".as_bytes().into());
+        map.insert("key1".as_bytes().into(), "0123456789012345678901234567890123456789Very_Long".as_bytes().into());
+        map.insert("key2bb".as_bytes().into(), "aval3".as_bytes().into());
+        map.insert("key2".as_bytes().into(), "short".as_bytes().into());
+        map.insert("key3cc".as_bytes().into(), "aval3".as_bytes().into());
+        map.insert("key3".as_bytes().into(), "1234567890123456789012345678901".as_bytes().into());
+
+        let mut database: HashMap<H256, Vec<u8>> = HashMap::new();
+        let mut trie: Trie<HashMap<H256, Vec<u8>>> = Trie::build(database, &map);
+
+        assert_eq!(trie.root(), H256::from_str("0xcb65032e2f76c48b82b5c24b3db8f670ce73982869d38cd39a624f23d62a9e89").unwrap());
+        assert_eq!(trie.get_raw("key2bb".as_bytes()), Some("aval3".as_bytes().into()));
+        assert_eq!(trie.get_raw("key2bbb".as_bytes()), None);
+        let prev_hash = trie.root();
+        trie.insert_raw("key2bbb".as_bytes().into(), "aval4".as_bytes().into());
+        assert_eq!(trie.get_raw("key2bbb".as_bytes()), Some("aval4".as_bytes().into()));
+        trie.remove_raw("key2bbb".as_bytes());
+        assert_eq!(trie.get_raw("key2bbb".as_bytes()), None);
+        assert_eq!(prev_hash, trie.root());
+    }
+}
