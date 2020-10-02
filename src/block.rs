8cab171b4c156a961ad175cc60192f6faf29e1ce
Wei Tang <wei@that.world>
2020-10-02 12:51:53 +0200

    Add helper functions to help generate ommers hash and transactions root

--- src/block.rs
+++ src/block.rs
@@ -1,5 +1,7 @@
 use alloc::vec::Vec;
 use rlp_derive::{RlpEncodable, RlpDecodable};
+use sha3::{Digest, Keccak256};
+use ethereum_types::H256;
 use crate::{Header, Transaction};
 
 #[derive(Clone, Debug, PartialEq, Eq, RlpEncodable, RlpDecodable)]
@@ -8,4 +10,25 @@ pub struct Block {
     pub header: Header,
     pub transactions: Vec<Transaction>,
     pub ommers: Vec<Header>,
+}
+
+impl Block {
+	pub fn new(
+		partial_header: Header,
+		transactions: Vec<Transaction>,
+		ommers: Vec<Header>,
+	) -> Self {
+		let ommers_hash = H256::from_slice(
+			Keccak256::digest(&rlp::encode_list(&ommers)[..]).as_slice(),
+		);
+		let transactions_root = H256::from_slice(
+			Keccak256::digest(&rlp::encode_list(&transactions)[..]).as_slice(),
+		);
+
+		Self {
+			header: Header::new(partial_header, ommers_hash, transactions_root),
+			transactions,
+			ommers,
+		}
+	}
 }
--- src/header.rs
+++ src/header.rs
@@ -3,6 +3,7 @@ use ethereum_types::{H160, H256, H64, U256, Bloom};
 
 #[derive(Clone, Debug, PartialEq, Eq, RlpEncodable, RlpDecodable)]
 #[cfg_attr(feature = "codec", derive(codec::Encode, codec::Decode))]
+/// Ethereum header definition.
 pub struct Header {
     pub parent_hash: H256,
     pub ommers_hash: H256,
@@ -19,4 +20,68 @@ pub struct Header {
     pub extra_data: H256,
     pub mix_hash: H256,
     pub nonce: H64,
+}
+
+impl Header {
+	pub fn new(
+		partial_header: Header,
+		ommers_hash: H256,
+		transactions_root: H256,
+	) -> Self {
+		Self {
+			parent_hash: partial_header.parent_hash,
+			ommers_hash,
+			beneficiary: partial_header.beneficiary,
+			state_root: partial_header.state_root,
+			transactions_root,
+			receipts_root: partial_header.receipts_root,
+			logs_bloom: partial_header.logs_bloom,
+			difficulty: partial_header.difficulty,
+			number: partial_header.number,
+			gas_limit: partial_header.gas_limit,
+			gas_used: partial_header.gas_used,
+			timestamp: partial_header.timestamp,
+			extra_data: partial_header.extra_data,
+			mix_hash: partial_header.mix_hash,
+			nonce: partial_header.nonce,
+		}
+	}
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+/// Partial header definition without ommers hash and transactions root.
+pub struct PartialHeader {
+    pub parent_hash: H256,
+    pub beneficiary: H160,
+    pub state_root: H256,
+    pub receipts_root: H256,
+    pub logs_bloom: Bloom,
+    pub difficulty: U256,
+    pub number: U256,
+    pub gas_limit: U256,
+    pub gas_used: U256,
+    pub timestamp: u64,
+    pub extra_data: H256,
+    pub mix_hash: H256,
+    pub nonce: H64,
+}
+
+impl From<Header> for PartialHeader {
+	fn from(header: Header) -> PartialHeader {
+		Self {
+			parent_hash: header.parent_hash,
+			beneficiary: header.beneficiary,
+			state_root: header.state_root,
+			receipts_root: header.receipts_root,
+			logs_bloom: header.logs_bloom,
+			difficulty: header.difficulty,
+			number: header.number,
+			gas_limit: header.gas_limit,
+			gas_used: header.gas_used,
+			timestamp: header.timestamp,
+			extra_data: header.extra_data,
+			mix_hash: header.mix_hash,
+			nonce: header.nonce,
+		}
+	}
 }
