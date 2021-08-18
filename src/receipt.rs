2f7bdaf3ba4e2aabf98230a38db44ea72bcc5912
Preston Evans <pbevans1@crimson.ua.edu>
2021-08-17 20:19:03 -0700

    Change receipt.state_root to U256
    This closes #19

--- src/receipt.rs
+++ src/receipt.rs
@@ -1,13 +1,13 @@
 use crate::Log;
 use alloc::vec::Vec;
-use ethereum_types::{Bloom, H256, U256};
+use ethereum_types::{Bloom, U256};
 use rlp_derive::{RlpDecodable, RlpEncodable};
 
 #[derive(Clone, Debug, PartialEq, Eq, RlpEncodable, RlpDecodable)]
 #[cfg_attr(feature = "with-codec", derive(codec::Encode, codec::Decode))]
 #[cfg_attr(feature = "with-serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Receipt {
-	pub state_root: H256,
+	pub state_root: U256,
 	pub used_gas: U256,
 	pub logs_bloom: Bloom,
 	pub logs: Vec<Log>,
