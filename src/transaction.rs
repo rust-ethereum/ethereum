cb1a1610b057120a180fbc3fb0f02401b37fb57f
tgmichel <telmo@purestake.com>
2022-01-04 15:48:53 +0100

    `storageKeys` serde alias for `AccessListItem` (#33)
    * `storageKeys` serde alias for `AccessListItem`
    
    * Bump `0.11.1`

--- src/transaction.rs
+++ src/transaction.rs
@@ -176,6 +176,7 @@ impl codec::Decode for TransactionSignature {
 #[cfg_attr(feature = "with-serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct AccessListItem {
 	pub address: Address,
+	#[cfg_attr(feature = "with-serde", serde(alias = "storageKeys"))]
 	pub slots: Vec<H256>,
 }
 
